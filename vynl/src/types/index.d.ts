ba4f3252affcf4a69a6807dbc17f1a8bd52d9cf7
lbernard18 <92778792+lbernard18@users.noreply.github.com>
2025-11-04 12:17:27 -0800

    First version of types (#7)
    Co-authored-by: louis.bernard18 <louis.bernard@epfl.ch>

---
+++ vynl/src/types/index.d.ts
@@ -0,0 +1,21 @@
+
+export interface User {
+  id: string;
+  name: string;
+  email: string; //Do we store that ?
+}
+
+export interface Song {
+  id: number;
+  title: string;
+  artist: string;
+  duration_sec: number;
+}
+
+export interface Playlist {
+  id: number;
+  name: string;
+  created_at: string;
+  user_id: string;
+  songs: Song[];
+}
