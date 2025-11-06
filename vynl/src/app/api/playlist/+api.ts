f1e5de4ce167a86526bcb4234e5e00f4b8a26d9a
Zack Crouse <zacktcrouse@gmail.com>
2025-11-05 21:52:59 -0800

    Partial create endpoint added

--- vynl/src/app/api/playlist/+api.ts
+++ vynl/src/app/api/playlist/+api.ts
@@ -1,10 +1,11 @@
 import { createSupabaseClient } from "@/src/server/supabase";
+import { isPlaylist } from "@/src/types";
+import { playlist_data } from "@/src/types/database";
 
 // GET "api/playlist"
 export async function GET(req: Request) {
     try {
         const supabase = await createSupabaseClient(req);
-        console.log("Supabase client created:", supabase);
 
         if (supabase instanceof Response) {
             return supabase
@@ -33,7 +34,39 @@ export async function GET(req: Request) {
     }
 }
 
-// PUT "api/playlist/:id"
-export async function PUT(req: Request) {
-    
+// CREATE "api/playlist"
+export async function CREATE(req: Request) {
+    try {
+        const body = await req.json();
+        const supabase = await createSupabaseClient(req);
+
+        if (supabase instanceof Response) {
+            return supabase
+        }
+
+        if (!isPlaylist(body)) {
+            return new Response('Invalid Body, expected playlist object', {
+                status: 400
+            });
+        }
+
+        const { data, error } = await supabase
+            .from('playlists')
+            // .insert({name: body.name, uid: body.user_id})
+            .select();
+        
+        if (error) {
+            return new Response('Failed to insert into database', {
+                status: 400
+            });
+        }
+
+        console.log(data)
+
+    } catch (error) {
+        console.error(error);
+        return new Response('Unknown Server Error', {
+            status: 500
+        });
+    }
 }
