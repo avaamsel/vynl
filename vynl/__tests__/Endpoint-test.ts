9a973af2e4846b1d03c3222c5e495511c695a08c
Zack Crouse <zacktcrouse@gmail.com>
2025-11-23 12:18:00 -0800

    Started on tests for get playlist by ID

--- vynl/__tests__/Endpoint-test.ts
+++ vynl/__tests__/Endpoint-test.ts
@@ -2,6 +2,7 @@ import { createSupabaseAdminClient } from "@/src/server/supabase";
 import { clearDatabase, createUser, deleteAllUsers } from "./utils/database";
 import { clear } from "console";
 import { POST as POST_PLAYLIST, GET as GET_PLAYLISTS } from "@/src/app/api/playlist/+api";
+import { GET as GET_PLAYLIST } from "@/src/app/api/playlist/[id]+api";
 import { isITunesPlaylist, ITunesPlaylist } from "@/src/types";
 import { Session, User } from "@supabase/supabase-js";
 import { emitTypingEvents } from "@testing-library/react-native/build/user-event/type/type";
@@ -174,6 +175,108 @@ describe('Test Backend', () => {
         });
 
         test('Get No Playlist', async () => {
+            const req = new Request("localhost:1234/api/playlist", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${session.access_token}`,
+                }
+            });
+
+            // Call get endpoint
+            const res = await GET_PLAYLISTS(req);
+            const body = await res.json();
+            expect(body).toStrictEqual([]);
+        });
+
+        test('Get One Playlist', async () => {
+            const filled_playlist = {
+                "id": 0, 
+                "name": "An empty playlist", 
+                "created_at": "", 
+                "user_id": user.id,
+                "songs": [
+                    { song_id: 12, title: "test song", artist: "fake artist", preview_url: "example.com", cover_url: "example.com", duration_sec: 51 },
+                    { song_id: 42, title: "test song 2", artist: "abcd", preview_url: "test.com/abcd", cover_url: "test.com/abcd", duration_sec: 231 },
+                    { song_id: 52, title: "test song 3", artist: "xyz", preview_url: "test.com/xyz", cover_url: "test.com/xyz", duration_sec: 192 }
+                ]
+            }
+
+            const body1 = await addPlaylist(filled_playlist, session.access_token);
+
+            const req2 = new Request("localhost:1234/api/playlist", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${session.access_token}`,
+                }
+            });
+            expect(req2).not.toBeNull();
+
+            const res2 = await GET_PLAYLISTS(req2);
+            const body2 = await res2.json();
+            expect(body2.length).toBe(1);
+            expect(isITunesPlaylist(body2[0])).toBeTruthy();
+            
+            // Our playlists should be the same
+            expect(body2[0]).toStrictEqual(body1);
+        });
+
+        test('Get Two Playlist', async () => {
+            const playlist1 = {
+                "id": 0, 
+                "name": "Favorites", 
+                "created_at": "", 
+                "user_id": user.id,
+                "songs": [
+                    { song_id: 56, title: "Love Story", artist: "Taylor Swift", preview_url: "example.com", cover_url: "example.com", duration_sec: 125 },
+                    { song_id: 2, title: "Lemon Tree", artist: "Mt. Joy", preview_url: "test.com/abcd", cover_url: "test.com/abcd", duration_sec: 70 },
+                    { song_id: 29, title: "Song Names Are Hard", artist: "xyz", preview_url: "test.com/xyz", cover_url: "test.com/xyz", duration_sec: 213 }
+                ]
+            }
+
+            const playlist2 = {
+                "id": 0, 
+                "name": "Not So Favorites", 
+                "created_at": "", 
+                "user_id": user.id,
+                "songs": [
+                    { song_id: 71, title: "test song 2", artist: "abcd", preview_url: "test.com/abcd", cover_url: "test.com/abcd", duration_sec: 231 },
+                    { song_id: 51, title: "test song", artist: "fake artist", preview_url: "example.com", cover_url: "example.com", duration_sec: 51 },
+                    { song_id: 4, title: "test song 3", artist: "xyz", preview_url: "test.com/xyz", cover_url: "test.com/xyz", duration_sec: 192 }
+                ]
+            }
+
+            const body1 = await addPlaylist(playlist1, session.access_token);
+            const body2 = await addPlaylist(playlist2, session.access_token);
+
+            const req = new Request("localhost:1234/api/playlist", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${session.access_token}`,
+                }
+            });
+            expect(req).not.toBeNull();
+
+            const res = await GET_PLAYLISTS(req);
+            const body = await res.json();
+            expect(body.length).toBe(2);
+            expect(isITunesPlaylist(body[0])).toBeTruthy();
+            expect(isITunesPlaylist(body[1])).toBeTruthy();
+            
+            // Our playlists should be the same
+            expect(body[0]).toStrictEqual(body1);
+            expect(body[1]).toStrictEqual(body2);
+        });
+    });
+
+    describe("Endpoint: GET 'api/playlist/:id'", async () => {
+        beforeEach(async () => {
+            await clearDatabase(adminClient);
+        });
+
+        test('Error: Get Nonexisting Playlist', async () => {
             const req = new Request("localhost:1234/api/playlist", {
                 method: "GET",
                 headers: {
