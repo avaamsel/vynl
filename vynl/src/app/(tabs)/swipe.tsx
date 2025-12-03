3e8ff79ab4038d0e953daa96e5b984812333624d
louis.bernard18 <louis.bernard@epfl.ch>
2025-12-02 17:10:12 -0800

    Displays an error message when no recommendations found, and goes back to home page

--- vynl/src/app/(tabs)/swipe.tsx
+++ vynl/src/app/(tabs)/swipe.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useRef, useState, useCallback } from 'react';
 import { Feather, FontAwesome } from '@expo/vector-icons';
-import { SafeAreaView, View, Text, StyleSheet, Dimensions, Animated, PanResponder, TouchableOpacity, TextInput, ScrollView } from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet, Dimensions, Animated, PanResponder, TouchableOpacity, TextInput, ScrollView, Alert } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Image } from 'expo-image';
 import * as Haptics from 'expo-haptics';
@@ -174,8 +174,16 @@ export default function Swiping() {
       });
 
       if (!res.ok) {
-        const text = await res.text();
-        console.error(text || 'Failed to fetch similar songs');
+        Alert.alert(
+          "No recommendations found",
+          "Please try with other songs"
+        );
+        await clearSession();
+        setActive(false);
+        await stopAll();
+        resetState();
+        router.push('/');
+        //TODO : delete the playlist
         return;
       }
 
